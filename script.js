6e9816222a8bfbb2c3f63fd5b2fd8c16e5faeea0
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-01-10 11:01:27 -0600

    This is the first exercise we have done

---
+++ script.js
@@ -0,0 +1,23 @@
+//JavaScript Basics Exercise
+
+console.log("250");
+//strings, numbers, boolean, null, and undefined are all primitives 
+//the six operator symbols are +, -, *, /, **, %
+console.log(1 % 5); // 1
+console.log(6 % 5); // 1
+console.log('I\'M MARRY POPPINS, Y\'ALL! ')
+console.log("Hawk" + " eye")
+console.log("Jack".length + " Hyde".length);
+console.log("Avengers: Endgame"[16])
+console.log("\\")
+console.log((5 ** 4)-9)
+console.log(("I ".length + "Am ".length + "Iron".length + " Man".length)%2 ); // 1
+//"CAP" "VISION" "OKOYE" "NICK FURY" "GROOT" "DOCTOR STRANGE" "THOR" "STAR-LORD")
+console.log("CAP");
+console.log("VISION");
+console.log("OKOYE")
+console.log("NICK FURY");
+console.log("GROOT");
+console.log("DOCTOR STRANGE");
+console.log("THOR");
+console.log("STAR-LORD");
